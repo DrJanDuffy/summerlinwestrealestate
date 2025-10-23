1d9a5273ccf276de21616d75889b3d1f8f781d6c
genekellyboyle <DrDuffy@bhhsnv.com>
2025-10-22 22:15:12 -0700

    fix(typescript): fix RealScoutOfficeListings props in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -197,9 +197,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               sortOrder="PRICE_LOW"
               listingStatus="For Sale"
               propertyTypes=",SFR,MF,TC,LAL,MOBILE,OTHER"
-              priceMin="600000"
-              priceMax="2000000"
-              maxListings={12}
+              priceMin={600000}
+              priceMax={2000000}
               className="mt-6"
             />
           </div>
